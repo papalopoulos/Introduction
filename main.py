bf5b09925dea088ecd5e62ec349b2f6d88f7c35c
papalopoulos <89782961+papalopoulos@users.noreply.github.com>
2023-02-07 15:14:38 +0200

    Rename main_.py to main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+from DC_01 import arithmetic_arranger
+
+arithmetic_arranger(["3801 - 2", "123 + 49","345 + 3"],True)
--- main_.py
+++
@@ -1,3 +0,0 @@
-from DC_01 import arithmetic_arranger
-
-arithmetic_arranger(["3801 - 2", "123 + 49","345 + 3"],True)
